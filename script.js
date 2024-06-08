7b0a7b3c2760059337ffabeb261c9a9d3e843dc2
hama <75150231+hamanyann@users.noreply.github.com>
2024-06-08 09:36:04 +0900

    0607

--- script.js
+++ script.js
@@ -21,80 +21,136 @@ const prefecturalCapital = [
   },
 ];
 
-document.addEventListener('DOMContentLoaded', questionSet);
-
-const checkButtons = document.querySelectorAll('.check');
+let correctAnswer = '';
+let remainingQuestions = [...prefecturalCapital];
 
 function questionSet() {
-  function getRandomIndex(max) {
-    return Math.floor(Math.random() * max);
-  }
-
-  function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-      const j = getRandomIndex(i + 1);
-      [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
-  }
-
-  function getRandomQuestion() {
-    const randomIndex = getRandomIndex(prefecturalCapital.length);
-    return prefecturalCapital[randomIndex];
-  }
-
-  function displayQuestion(questionObj) {
-    document.getElementById('question-text').textContent = questionObj.question;
-    const shuffledAnswers = shuffleArray([...questionObj.answers]);
-
-    const buttons = document.querySelectorAll('.check');
-    buttons.forEach((button, index) => {
-      button.textContent = shuffledAnswers[index];
-      button.dataset.correct =
-        shuffledAnswers[index] === questionObj.answers[0];
+  if (remainingQuestions.length === 1) {
+    const endButtons = document.querySelectorAll('.end-button');
+    nextButtons.forEach((nextButton, index) => {
+      nextButton.textContent = '結果';
+      nextButton.removeEventListener('click', leStart);
+      nextButton.addEventListener('click', showResult);
     });
   }
 
-  const randomQuestion = getRandomQuestion();
+  function showResult() {
+    const endCard = document.querySelector('.endCard');
+    endCard.classList.remove('hidden');
+    if (totalPointCount == prefecturalCapital.length) {
+      document.querySelector(
+        '.endCard-serif'
+      ).innerHTML = `${prefecturalCapital.length}問中${totalPointCount}問正解<br>おめでとう！`;
+    } else {
+      document.querySelector(
+        '.endCard-serif'
+      ).innerHTML = `${prefecturalCapital.length}問中${totalPointCount}問正解<br>お疲れ様！`;
+    }
+  }
+
+  const randomIndex = getRandomIndex(remainingQuestions.length);
+  const randomQuestion = remainingQuestions[randomIndex];
+  remainingQuestions.splice(randomIndex, 1);
   displayQuestion(randomQuestion);
+  console.log(remainingQuestions);
+}
+
+function getRandomIndex(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = getRandomIndex(i + 1);
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+document.addEventListener('DOMContentLoaded', questionSet);
+
+function displayQuestion(questionObj) {
+  document.getElementById(
+    'question-text'
+  ).textContent = `Q. ${questionObj.question}`;
+  const shuffledAnswers = shuffleArray([...questionObj.answers]);
+
+  const buttons = document.querySelectorAll('.check');
+  buttons.forEach((button, index) => {
+    button.textContent = shuffledAnswers[index];
+    button.dataset.correct = shuffledAnswers[index] === questionObj.answers[0];
+  });
+  correctAnswer = questionObj.answers[0];
+  totalPoint.textContent = `${prefecturalCapital.length}問中0問正解`;
+  document.querySelector(
+    '.correct-answer-first'
+  ).textContent = `A. ${correctAnswer}`;
 }
 
 const correct = document.querySelector('.correct');
 const incorrect = document.querySelector('.incorrect');
+const correctAnswerFirst = document.querySelector('.correct-answer-first');
+const correctAnswerSecond = document.querySelector('.correct-answer-second');
+const serif = document.querySelector('.serif');
+const totalPoint = document.querySelector('.total-point');
 const correctImg1 = document.querySelector('.img1');
 const correctImg2 = document.querySelector('.img2');
 const correctImg3 = document.querySelector('.img3');
 const correctImg4 = document.querySelector('.img4');
+let totalPointCount = 0;
 
 document.addEventListener('click', function (event) {
   if (event.target.classList.contains('check')) {
     if (event.target.dataset.correct === 'true') {
       console.log('正解!');
       correct.classList.remove('hidden');
-      correctImg1.classList.add('hidden');
       correctImg2.classList.remove('hidden');
-      document.querySelector('.serif').textContent = 'すごい！';
+      serif.textContent = 'すごい！';
+      totalPointCount += 1;
+      correctIncorrect();
     } else {
       console.log('不正解...');
       incorrect.classList.remove('hidden');
-      correctImg1.classList.add('hidden');
-      correctImg3.classList.remove('hidden');      
-      document.querySelector('.serif').textContent = 'うにゃ';
+      correctImg3.classList.remove('hidden');
+      serif.textContent = 'うにゃ';
+      correctIncorrect();
     }
   }
 });
 
-const imgButtons = document.querySelectorAll('.img');
+function correctIncorrect() {
+  correctImg1.classList.add('hidden');
+  correctAnswerFirst.classList.remove('hidden');
+  correctAnswerSecond.classList.add('hidden');
+  totalPoint.textContent = `${prefecturalCapital.length}問中${totalPointCount}問正解`;
+  questionSet();
+}
 
+function leStart() {
+  console.log(`wa`);
+  correct.classList.add('hidden');
+  incorrect.classList.add('hidden');
+  correctImg1.classList.remove('hidden');
+  correctImg2.classList.add('hidden');
+  correctImg3.classList.add('hidden');
+
+  
+  document.querySelector('.serif').textContent = 'うーん...';
+  correctAnswerFirst.classList.add('hidden');
+  correctAnswerSecond.classList.remove('hidden');
+  totalPoint.textContent = `${prefecturalCapital.length}問中${totalPointCount}問正解`;
+}
+
+const imgButtons = document.querySelectorAll('.img');
 imgButtons.forEach((imgButton, index) => {
   imgButton.addEventListener('click', function () {
-    console.log(`wa`);
-    correct.classList.add('hidden');
-    incorrect.classList.add('hidden');
-    correctImg1.classList.remove('hidden');
-    correctImg2.classList.add('hidden');
-    correctImg3.classList.add('hidden');
-    questionSet();
-    document.querySelector('.serif').textContent = 'うーん...';
+    leStart();
+  });
+});
+
+const nextButtons = document.querySelectorAll('.next-button');
+nextButtons.forEach((nextButton, index) => {
+  nextButton.addEventListener('click', function () {
+    leStart();
   });
 });
